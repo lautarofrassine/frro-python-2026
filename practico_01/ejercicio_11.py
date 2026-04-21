24062439ad1d0942f97af881d888627dabef3591
facugreg <gregoretfacu18@gmail.com>
2026-04-21 17:29:32 -0300

    Resolución ejercicio 11 practico 01

--- practico_01/ejercicio_11.py
+++ practico_01/ejercicio_11.py
@@ -10,7 +10,16 @@ def suma_cubo_pares_for(numeros: Iterable[int]) -> int:
     Restricción: Utilizar dos bucles for, uno para elevar al cubo y otro para
     separar los pares.
     """
-    pass # Completar
+    suma:int=0
+    numeros_cubo=[]
+    for n in numeros:
+        c=n**3
+        numeros_cubo.append(c)
+    for n in numeros_cubo:
+        if n%2==0:
+            suma=suma+n
+    return suma
+
 
 
 # NO MODIFICAR - INICIO
@@ -28,7 +37,7 @@ def suma_cubo_pares_sum_list(numeros: Iterable[int]) -> int:
     Referencia: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     Referencia: https://docs.python.org/3/library/functions.html#sum
     """
-    pass # Completar
+    return sum([n**3 for n in numeros if n%2==0])
 
 
 # NO MODIFICAR - INICIO
@@ -44,7 +53,7 @@ def suma_cubo_pares_sum_gen(numeros: Iterable[int]) -> int:
     y la función sum.
     Referencia: https://docs.python.org/3/reference/expressions.html#generator-expressions
     """
-    pass # Completar
+    return sum(n**3 for n in numeros if n%2==0)
 
 
 # NO MODIFICAR - INICIO
@@ -64,25 +73,25 @@ numeros = [1, 2, 3, 4, 5, 6]
 
 # Escribir una función lambda que eleve los elementos al cubo
 
-numeros_al_cubo = # Completar
+numeros_al_cubo = (lambda num:[n**3 for n in num  ])(numeros)
 
 
 # Escribir una función lambda que permita filtrar todos los elementos pares
 
-numeros_al_cubo_pares = # Completar
+numeros_al_cubo_pares = (lambda num:[n**3 for n in num if n%2==0])(numeros)
 
 
 # Escribir una función Lambda que sume todos los elementos
 
 from functools import reduce
 
-suma_numeros_al_cubo_pares = # Completar
+suma_numeros_al_cubo_pares = (lambda num:reduce(lambda a,b:a+b,num))(numeros_al_cubo_pares)
 
 
 # Escribir una función Lambda que permita ordenar los elementos de la numeros
 # en base a si son pares o impares
 
-numeros_ordenada = # Completar
+numeros_ordenada = (lambda num:[n for n in num if n%2!=0]+[n for n in num if n%2==0])(numeros)
 
 # NO MODIFICAR - INICIO
 assert numeros_al_cubo == [1, 8, 27, 64, 125, 216]
