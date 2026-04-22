9690293b8452c499a651c7ab658e009865d5374a
Lautaro Frassine <lautifrassine@gmail.com>
2026-04-22 00:18:56 -0300

    Resolución ejercicio 7

--- practico_01/ejercicio_07.py
+++ practico_01/ejercicio_07.py
@@ -8,7 +8,8 @@ def es_palindromo(palabra: str) -> bool:
     Restricción: No utilizar bucles - Usar Slices de listas.
     Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
     """
-    pass # Completar
+    palabra = palabra.lower()
+    return palabra == palabra[::-1]
 
 
 # NO MODIFICAR - INICIO
@@ -28,7 +29,10 @@ def mitad(palabra: str) -> str:
     Restricción: No utilizar bucles - Usar Slices de listas.
     Referencia: https://docs.python.org/3/tutorial/introduction.html#lists
     """
-    pass # Completar
+    if len(palabra) % 2 == 0:
+        return palabra[:len(palabra) // 2]
+    else:
+        return palabra[:len(palabra) // 2 + 1]
 
 
 # NO MODIFICAR - INICIO
