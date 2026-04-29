33d58c42046f86b9b5004bf96921a38e3aa7319e
Lautaro Frassine <lautifrassine@gmail.com>
2026-04-29 14:55:59 -0300

    Resolución ejercicio 5

--- practico_01/ejercicio_05.py
+++ practico_01/ejercicio_05.py
@@ -9,7 +9,13 @@ def multiplicar_basico(numeros: Iterable[float]) -> float:
 
     Restricciones: No usar bibliotecas auxiliares (Numpy, math, pandas).
     """
-    pass # Completar
+    if not numeros:
+        return 0
+
+    producto = 1
+    for n in numeros:
+        producto *= n
+    return producto
 
 
 # NO MODIFICAR - INICIO
@@ -31,7 +37,9 @@ def multiplicar_reduce(numeros: Iterable[float]) -> float:
     """CHALLENGE OPCIONAL - Re-escribir utilizando reduce.
     Referencia: https://docs.python.org/3.8/library/functools.html#functools.reduce
     """
-    pass # Completar
+    if not numeros:
+        return 0
+    return reduce(lambda x, y: x * y, numeros)
 
 
 # NO MODIFICAR - INICIO
